3f42acef0e8b49879899f75c63fd0f4d492c2273
nnnnn81 <nnnnnagi81@gmail.com>
2024-03-18 20:49:09 +0900

    add comment

--- app/router/user.py
+++ app/router/user.py
@@ -2,19 +2,22 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-
+# ユーザー新規登録
 @router.post("/signup")
 async def Signup():
     pass
 
+# ユーザーログイン
 @router.post("/login")
 async def Login():
     pass
 
+# ユーザー情報取得
 @router.get("/user/{userid}")
 async def userinfo():
     pass
 
+# ユーザー情報アップデート
 @router.patch("/user/{userid}")
 async def update_user():
     pass
